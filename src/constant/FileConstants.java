3e370f29f29fabcd0a6a7301cd8f9b2ce7b8866d
DimkaKazak <Kazak211099@gmail.com>
2020-05-21 20:07:46 +0300

    add: swear words

--- src/constant/FileConstants.java
+++ src/constant/FileConstants.java
@@ -6,5 +6,7 @@ public class FileConstants {
 
     private static final String sep = File.separator;
 
-    public static final String CONFIG_FILE_PATH = "src" + sep + "resourses" + sep + "config.properties";
+    public static final String CONFIG_FILE_PATH = "src" + sep + "resources" + sep + "config.properties";
+    public static final String RUS_SWEAR_WORDS_FILE_PATH = "src" + sep + "resources" + sep + "words.txt";
+    public static final String PUNCTUATION_MARKS = "src" + sep + "resources" + sep + "punctuationMarks.txt";
 }
