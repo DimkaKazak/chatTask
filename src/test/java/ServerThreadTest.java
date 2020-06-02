ffde2360e263b2b4f75b6e0f20f32f928f6d3bc8
DimkaKazak <Kazak211099@gmail.com>
2020-06-02 18:42:02 +0300

    refactor: tests (make pool for client Threads)

--- src/test/java/ServerThreadTest.java
+++ src/test/java/ServerThreadTest.java
@@ -1,30 +1,10 @@
-import base.client.ClientThread;
 import base.server.ServerTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.PrintWriter;
-
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 
 public class ServerThreadTest extends ServerTest {
-
-    @BeforeClass
-    public void initClientsOnServer(){
-        int j = 0;
-
-        for (ClientThread clientThread : clientThreadList){
-
-            PrintWriter out;
-            int counter = 0;
-            do {
-                out = clientThread.getClient().getOut();
-                counter++;
-            } while (out == null || counter != 1000);
-
-            out.println("RandomName" + j);
-            j++;
-        }
-    }
 
     @Test(threadPoolSize = POOL_SIZE, invocationCount = INVOCATION_COUNT, invocationTimeOut = INVOCATION_TIME_OUT)
     public void testConnections() {
--- src/test/java/base/server/ServerTest.java
+++ src/test/java/base/server/ServerTest.java
@@ -1,55 +1,38 @@
 package base.server;
 
-import base.client.ClientThread;
 import chat.client.Client;
 import chat.server.Server;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
-public class ServerTest extends Assert {
-
-    protected ServerThread serverThread;
-    protected List<ClientThread> clientThreadList = new LinkedList();
+public class ServerTest {
 
     protected final int POOL_SIZE = 30;
     protected final int INVOCATION_COUNT = 1;
     protected final int INVOCATION_TIME_OUT = 10000;
 
+    protected ServerThread serverThread;
+    protected ExecutorService clientPool;
+
     @BeforeClass
     public void setUpSpace(){
+
         serverThread = new ServerThread(new Server());
-
-        for (int i = 0; i < POOL_SIZE; i++){
-            clientThreadList.add(new ClientThread(new Client()));
-        }
-
         serverThread.start();
 
-        for (ClientThread clientThread : clientThreadList){
-            clientThread.start();
+        clientPool = Executors.newFixedThreadPool(POOL_SIZE);
+        Runnable clientInitTask = () -> new Client().startClient();
+        for (int i = 0; i < POOL_SIZE; i++){
+            clientPool.execute(clientInitTask);
         }
     }
 
     @AfterClass
     public void setDownSpace(){
-
-        for (ClientThread clientThread : clientThreadList){
-            if (!clientThread.isInterrupted()){
-                clientThread.interrupt();
-            }
-            clientThread.setClient(null);
-        }
-        clientThreadList = null;
-
-        if (!serverThread.isInterrupted()){
-            serverThread.interrupt();
-        }
-        serverThread.setServer(null);
-        serverThread = null;
+        clientPool.shutdown();
     }
 }
