458a178fbda41219bc5b3b60a1e16848f938e979
DimkaKazak <Kazak211099@gmail.com>
2020-06-17 19:30:05 +0300

    refactor: filter resourses

--- src/main/java/constant/FileConstants.java
+++ src/main/java/constant/FileConstants.java
@@ -7,8 +7,8 @@ public class FileConstants {
     private static final String sep = File.separator;
 
     public static final String CONFIG_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "config.properties";
-    public static final String RUS_SWEAR_WORDS_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "swearWords.txt";
-    public static final String NAMES_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "namesRUS.txt";
-    public static final String CAPITALS_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "capitalsENG.txt";
-    public static final String COUNTRY_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "countryENG.txt";
+    public static final String RUS_SWEAR_WORDS_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "filters/swearWords.txt";
+    public static final String NAMES_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "filters/namesRUS.txt";
+    public static final String CAPITALS_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "filters/capitalsENG.txt";
+    public static final String COUNTRY_FILE_PATH = "src" + sep + "main" + sep + "resources" + sep + "filters/countryENG.txt";
 }
